a80090836d0ecc46cbbc6f5bc6e7d413a5d4269d
Austin Griffith <austin@ethereum.org>
2025-12-30 19:16:27 -0700

    Move MockUSDC to separate file
    Fixes 'Multiple contracts in target path' error during deploy

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.24;
 
 import "./DeployHelpers.s.sol";
 import "../contracts/HousePool.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../contracts/MockUSDC.sol";
 
 /**
  * @notice Main deployment script for HousePool
  * @dev Run this when you want to deploy: yarn deploy
  * 
  * Works on both:
- * - Local chain (yarn chain) - uses MockUSDC
+ * - Local chain (yarn chain) - deploys MockUSDC
  * - Base fork (yarn fork --network base) - uses real USDC
  */
 contract DeployScript is ScaffoldETHDeploy {
@@ -59,69 +59,5 @@ contract DeployScript is ScaffoldETHDeploy {
             size := extcodesize(addr)
         }
         return size > 0;
-    }
-}
-
-/// @title MockUSDC - Simple mock for local testing
-contract MockUSDC is IERC20 {
-    string public constant name = "USD Coin";
-    string public constant symbol = "USDC";
-    uint8 public constant decimals = 6;
-    
-    uint256 private _totalSupply;
-    mapping(address => uint256) private _balances;
-    mapping(address => mapping(address => uint256)) private _allowances;
-    
-    constructor() {
-        // Mint 1M USDC to deployer
-        _mint(msg.sender, 1_000_000 * 10**6);
-    }
-    
-    function mint(address to, uint256 amount) external {
-        _mint(to, amount);
-    }
-    
-    function totalSupply() external view override returns (uint256) {
-        return _totalSupply;
-    }
-    
-    function balanceOf(address account) external view override returns (uint256) {
-        return _balances[account];
-    }
-    
-    function transfer(address to, uint256 amount) external override returns (bool) {
-        _transfer(msg.sender, to, amount);
-        return true;
-    }
-    
-    function allowance(address owner, address spender) external view override returns (uint256) {
-        return _allowances[owner][spender];
-    }
-    
-    function approve(address spender, uint256 amount) external override returns (bool) {
-        _allowances[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
-        return true;
-    }
-    
-    function transferFrom(address from, address to, uint256 amount) external override returns (bool) {
-        uint256 currentAllowance = _allowances[from][msg.sender];
-        require(currentAllowance >= amount, "ERC20: insufficient allowance");
-        _allowances[from][msg.sender] = currentAllowance - amount;
-        _transfer(from, to, amount);
-        return true;
-    }
-    
-    function _transfer(address from, address to, uint256 amount) internal {
-        require(_balances[from] >= amount, "ERC20: insufficient balance");
-        _balances[from] -= amount;
-        _balances[to] += amount;
-        emit Transfer(from, to, amount);
-    }
-    
-    function _mint(address to, uint256 amount) internal {
-        _totalSupply += amount;
-        _balances[to] += amount;
-        emit Transfer(address(0), to, amount);
     }
 }
